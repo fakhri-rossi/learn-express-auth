152b0cd2c5857b349b2c04632d49fcf4fe987e85
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-20 17:32:43 +0700

    Created Register feature username and password with bcrypt to hash the password

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://127.0.0.1/auth_demo');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// middleware to gather data from req.body
+app.use(express.urlencoded({
+    extended: true
+}));
+
 const User = require('./models/user');
 
-app.get('/register', (req, res) => {
-    res.render('register')
+app.get('/', (req, res) => {
+    res.send('homepage');
 });
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    
+    const user = new User({
+        username,
+        password: hashedPassword
+    });
+
+    await user.save();
+    console.log(user);
+    res.redirect('/');
+});
+
 
 app.get('/admin', (req, res) => {
     res.send('Halaman ini hanya bisa diakses jika kamu login');
